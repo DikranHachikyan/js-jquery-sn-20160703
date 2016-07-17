f168b48b281d5842ef124d29b08e113bbe53c930
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-07-17 20:29:31 +0300

    Lesson 05

---
+++ js/pictures.js
@@ -0,0 +1,35 @@
+var images = [
+    {
+      "url": "http://btv.bg",
+      "src": "pic2.jpg",
+      "thumbnail": "pic2.jpg",
+      "title": "Millennium Monument",
+      "alt": "Millennium Monument",
+      "info": "Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipisicing elit. Iste veniam quis quam <em>numquam</em> ex, ipsam recusandae placeat officiis odio magnam provident tempora nemo sit nostrum voluptates libero, animi. Aliquid, ex?"
+    },
+    {
+      "url": "http://dir.bg",
+      "src": "pic1.jpg",
+      "thumbnail": "pic1.jpg",
+      "title": "City Center",
+      "alt": "City Center",
+      "info": "Necessitatibus amet <strong>magnam</strong> neque quasi, hic fugiat unde consequatur. Aspernatur a facilis perferendis, earum, quia qui officiis iure placeat, quod magni temporibus! Dolor est fugiat architecto at sit placeat non."
+    },
+    {
+      "url": "http://abv.bg",
+      "src": "pic3.jpg",
+      "thumbnail": "pic3.jpg",
+      "title": "City Attractions",
+      "alt": "City Attractions",
+      "info": "Ea numquam <strong>aliquam quasi</strong> maiores soluta quidem debitis est repudiandae, architecto neque dignissimos. Repellat numquam nulla atque enim, necessitatibus maiores quibusdam culpa sit est consequatur laboriosam error, molestiae harum facere."
+    },
+    {
+      "url": "http://linux-bg.org",
+      "src": "pic4.jpg",
+      "thumbnail": "pic4.jpg",
+      "title": "Avon River",
+      "alt": "Avon River",
+      "info": "Nemo temporibus <strong>molestiae</strong> consequuntur dolores sed officiis perferendis iusto doloremque voluptatem modi sint rerum vero neque quidem laudantium, itaque, excepturi <em>vitae</em> quas voluptas placeat. Deserunt hic illo modi dolore perferendis."
+    }
+  
+];
